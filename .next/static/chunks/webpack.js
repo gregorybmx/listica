752b3dc3f020b60717263cb4ab65bbfcdf8ac02c
Greivin Montoya Estrada <greivin.montoya.estrada@est.una.ac.cr>
2022-05-01 16:11:25 -0600

    Image Tag en Componente Servicios

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "30cf0321e3017fe8"; }
+/******/ 		__webpack_require__.h = function() { return "fa51aca47404dfdb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Placeholder.jsx
+++ components/Placeholder.jsx
@@ -1,11 +1,13 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 import styles from '../styles/Placeholder.module.css'
+import Image from 'next/image'
 
 const Placeholder = (props) => {
     return (
         <div id={styles['placeholder']} className="card">
-            <img id={styles['imagen']} src={props.img} className="card-img-top" alt="MobileAppsImage"/>
+            <div id={styles['imagenContainer']}>
+                <Image src={props.img} layout="responsive" width={39} height={38} className="card-img-top" alt="MobileAppsImage"/>
+            </div>
             <div id={styles['contenido']} className="card-body">
                 <h5 id={styles['titulo']} className="card-title">{props.title}</h5>
                 <p id={styles['texto']} className="card-text">{props.text}</p>
