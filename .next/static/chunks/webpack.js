43e52f3e43aa66e5d248ec75d89491f93381c66b
Greivin Montoya Estrada <greivin.montoya.estrada@est.una.ac.cr>
2022-05-02 00:17:22 -0600

    Commit merge

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2d4089bf78981524"; }
+/******/ 		__webpack_require__.h = function() { return "d17d3a016c67ebda"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
